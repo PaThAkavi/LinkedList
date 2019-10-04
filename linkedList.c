b017fece7ae010d0df46d385e35def2edc1f3f3c
Avaneesh Pathak <pathakavaneesh@gmail.com>
2019-10-04 12:31:01 +0530

    added a new function to calculate list length

--- linkedList.c
+++ linkedList.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 void append();
+void display();
+int length();
 
 struct node{
     int data;
@@ -11,6 +13,10 @@ struct node *root = NULL;
 
 int main(){
     append();
+    append();
+    append();
+    display();
+    printf("%d\n", length());
     return 0;
 }
 
@@ -19,6 +25,7 @@ void append(){
     newnode = (struct node *)malloc(sizeof(struct node));
     printf("Enter the node value : \n");
     scanf("%d", &newnode->data);
+    newnode->link = NULL;
     if(root==NULL){
         root = newnode;
     }
@@ -29,5 +36,37 @@ void append(){
             ptr = ptr->link;
         }
         ptr->link = newnode;
+    }
+}
+
+void display(){
+    printf("The elements of the linked list are : \n");
+    if(root == NULL){
+        printf("There are no elements in the list as of now. \n");
+    }
+    else{
+        struct node *ptr;
+        ptr = root;
+        while(ptr->link != NULL){
+            printf("%d->", ptr->data);
+            ptr = ptr->link;
+        }
+        printf("\n");
+    }
+}
+
+int length(){
+    if(root == NULL){
+        return 0;
+    }
+    else{
+        struct node *ptr;
+        ptr = root;
+        int count = 0;
+        while(ptr->link != NULL){
+            ptr = ptr->link;
+            count ++ ;
+        }
+        return count;
     }
 }
