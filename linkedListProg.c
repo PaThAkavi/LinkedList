bc32481c9cc062dea0b130d6583177f3e526126d
Avaneesh Pathak <pathakavaneesh@gmail.com>
2019-10-04 22:31:13 +0530

    Added more deletion properties

--- linkedListProg.c
+++ linkedListProg.c
@@ -1,3 +1,4 @@
+// SINGLE LINKED LIST
 #include<stdio.h>
 #include<stdlib.h>
 void addAtEnd();
@@ -6,7 +7,9 @@ void display();
 int length();
 void deleteBeg();
 void deleteEnd();
-void deleteParticular(int);
+void deleteParticular();
+void addAfter();
+void addBefore();
 
 struct node{
     int data;
@@ -17,25 +20,46 @@ struct node *root = NULL;
 
 int main(){
     while(1){
-        printf("Single linked list operations : \n");
         int option;
+        printf("Single linked list operations : \n");
         printf("1. Add element at the end \n");
         printf("2. Add element in the beginning \n");
-        printf("3. display all the elements \n");
-        printf("4. Find the length of the list \n");
-        printf("5. Delete node from begenning \n");
-        printf("6. Delete node from end \n");
-        printf("7. Exit\n");
+        printf("3. Add element after a specified node \n");
+        printf("4. Add element before a specified node \n");
+        printf("5. display all the elements \n");
+        printf("6. Find the length of the list \n");
+        printf("7. Delete node from begenning \n");
+        printf("8. Delete node from end \n");
+        printf("9. Delete a particular node. \n");
+        printf("10. Exit\n");
+        printf("\n");
+        printf("Enter your choice : \n");
         scanf("%d", &option);
 
         switch(option){
-            case 1: addAtEnd(); printf("--------------------------------------\n"); break;
-            case 2: addAtBeg(); printf("--------------------------------------\n"); break;
-            case 3: display(); printf("--------------------------------------\n"); break;
-            case 4: printf("The number of elements in the list : %d\n",length()); printf("--------------------------------------\n"); break;
-            case 5: deleteBeg(); break;
-            case 6: deleteEnd(); break;
-            case 7: exit(0);
+            case 1: addAtEnd(); 
+                    printf("--------------------------------------\n"); 
+                    break;
+            case 2: addAtBeg(); 
+                    printf("--------------------------------------\n"); 
+                    break;
+            case 3: addAfter(); 
+                    break;
+            case 4: addBefore(); 
+                    break;
+            case 5: display(); 
+                    printf("--------------------------------------\n"); 
+                    break;
+            case 6: printf("The number of elements in the list : %d\n",length()); 
+                    printf("--------------------------------------\n"); 
+                    break;
+            case 7: deleteBeg(); 
+                    break;
+            case 8: deleteEnd(); 
+                    break;
+            case 9: deleteParticular(); 
+                    break;
+            case 10: exit(0);
             default: printf("Invalid input"); printf("--------------------------------------\n");
         }
     }
@@ -95,11 +119,11 @@ int length(){
 }
 
 void display(){
-    printf("The elements in the list are : \n");
     if(root == NULL){
         printf("No elements present.\n");
     }
     else{
+        printf("The elements in the list are : \n");
         struct node *ptr;
         ptr = root;
         while(ptr != NULL){
@@ -111,20 +135,93 @@ void display(){
 }
 
 void deleteBeg(){
-    struct node *ptr;
-    ptr = root;
-    root = ptr->link;
+    if(root == NULL){
+        printf("There are currently no elements in the list to delete. \n");
+        printf("----------------------------------------------------------\n");
+    }
+    else{
+        struct node *ptr;
+        ptr = root;
+        root = ptr->link;
+    }
+    
 }
 
 void deleteEnd(){
-    struct node *ptr;
-    ptr = root;
-    while(ptr->link->link != NULL){
-        ptr = ptr->link;
+    if(root == NULL){
+        printf("There are currently no elements in the list to delete. \n");
+        printf("----------------------------------------------------------\n");
     }
-    ptr->link = NULL;
+    else{
+        struct node *ptr;
+        ptr = root;
+        while(ptr->link->link != NULL){
+            ptr = ptr->link;
+        }
+        ptr->link = NULL;
+    }
 }
 
-void deleteParticular(int ele){
+void deleteParticular(){
+    int ele;
+    if(root == NULL){
+        printf("There are currently no elements in the list to delete. \n");
+        printf("----------------------------------------------------------\n");
+    }
+    else{
+        printf("Enter the element to be deleted : \n");
+        scanf("%d", &ele);
+        struct node *ptr;
+        ptr = root;
+        while(ptr->link->data != ele){
+            ptr = ptr->link;
+        }
+        ptr->link = ptr->link->link;
+    }
+}
 
+void addAfter(){
+    int ele;
+    struct node *newnode;
+    newnode = (struct node *)malloc(sizeof(struct node));
+    printf("Enter the data value to be inserted : \n");
+    scanf("%d", &newnode->data);
+    if(root == NULL){
+        newnode->link = NULL;
+        root = newnode;
+    }
+    else{
+        printf("Enter the element after which insertion will take place : \n");
+        scanf("%d", &ele);
+        struct node *ptr;
+        ptr = root;
+        while(ptr->data != ele){
+            ptr = ptr->link;
+        }
+        newnode->link = ptr->link;
+        ptr->link = newnode;
+    }
+}
+
+void addBefore(){
+    int ele;
+    struct node *newnode;
+    newnode = (struct node *)malloc(sizeof(struct node));
+    printf("Enter the data value to be inserted : \n");
+    scanf("%d", &newnode->data);
+    if(root == NULL){
+        newnode->link = NULL;
+        root = newnode;
+    }
+    else{
+        printf("Enter the element before which deletion will take place : \n");
+        scanf("%d", &ele);
+        struct node *ptr;
+        ptr = root;
+        while(ptr->link->data != ele){
+            ptr = ptr->link;
+        }
+        newnode->link = ptr->link;
+        ptr->link = newnode;
+    }
 }
